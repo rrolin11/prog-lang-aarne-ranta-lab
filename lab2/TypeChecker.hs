237ac79ad4d7aff410a0e7abb5d41d4885c09097
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-05-29 21:21:01 -0300

    cambios en las funciones

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -12,42 +12,97 @@ import Prelude hiding (fail)
 #endif
 
 typecheck :: Program -> Err ()
-typecheck p = do 
-    env <- emptyEnv
-    return checkProg env p
+typecheck prog = case prog of
+    [] -> fail $ "el programa es vacío"
+    defs -> do 
+        env <- emptyEnv
+        return checkProg env prog
+
+loadFunctionToEnv :: [Def] -> Env -> Env
+loadFunctionToEnv [] env = env
+loadFunctionToEnv (DFun type id args stms):defs env = do
+        types <- [t | (ADecl t id) <- args]
+        env' <- updateFun env id (types,type)
+        loadFunctionToEnv defs env'
 
 checkProg :: Env -> Program -> Err ()
 checkProg env prog = case prog of
     [] -> fail $ "el programa es vacío"
-    defs -> return checkDefs env defs
+    defs -> do 
+        env <- loadFunctionToEnv defs env
+        checkDefs env defs
 
 checkDefs :: Env -> [Def] -> Err ()
 checkDefs env defs = case defs of
-    [] -> fail $ "la lista de definiciones es vacía"
-    def:[] -> checkDef env def
-    def:defs -> -- IMPLEMENTAR
+    env [] -> ()
+    env def:defs -> do
+        _ <- checkDef env def
+        checkDefs env defs
+
+loadArgsToEnv :: [Arg] -> Env -> Env
+loadArgsToEnv [] env = env
+loadArgsToEnv (ADecl type id):args env = do
+        env' <- updateVar env [id] type
+        loadArgsToEnv args env'
 
 checkDef :: Env -> Def -> Err ()
-checkDef env def -> case def of 
-    DFun type id args stms -> do 
-        updateFun env id -- IMPLEMENTAR
-
+checkDef env (DFun type id args stms) = do
+    env' <- loadArgsToEnv args env
+    _ <- checkStms type env' stms
+    return ()
+    
 inferExp :: Env -> Exp -> Err Type
 inferExp env x = case x of
     ETrue -> return Type_bool
+    EFalse -> return Type_bool
     EInt n -> return Type_int
-    EId id -> lookupVar env id
-    EAdd exp1 exp2 ->
-        inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+    EDouble n -> return Type_double
+    EString s -> return Type_string
+    EId id -> lookupVar env id 
+    EApp id exps -> do
+       (types,type) <- lookupFun env id
+       typesExp <- [inferExp env e | e <- exps]
+       if (types == typesExp) then
+            return type
+       else
+            fail $ "alguno de los tipos no coincide"   
+    EPIncr exp -> inferExp env exp
+    EPDecr exp -> inferExp env exp
+    EIncr exp -> inferExp env exp
+    EDecr exp -> inferExp env exp
+    ETimes exp1 exp2 -> inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+    EDiv exp1 exp2 -> inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+    EPlus exp1 exp2 -> inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+    EMinus exp1 exp2 -> inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+    ELt exp1 exp2 -> do
+        _ <- inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+        return Type_bool
+    EGt exp1 exp2 -> do
+        _ <- inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+        return Type_bool
+    ELtEq exp1 exp2 -> do
+        _ <- inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+        return Type_bool
+    EGtEq exp1 exp2 -> do
+        _ <- inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+        return Type_bool
+    EEq exp1 exp2 -> do
+        _ <- inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+        return Type_bool
+    ENEq exp1 exp2 -> do
+        _ <- inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+        return Type_bool
+    EAnd exp1 exp2 -> do
+        _ <- inferBin [Type_bool] env exp1 exp2
+        return Type_bool
+    EOr exp1 exp2 -> do
+        _ <- inferBin [Type_bool] env exp1 exp2
+        return Type_bool
+    EAss exp1 exp2 -> do
+        _ <- inferBin [Type_int, Type_double, Type_string] env exp1 exp2
+        inferExp env exp1
+    ETyped exp type -> fail $ "Fallo del ETyped "
 
-inferBin :: [Type] -> Env -> Exp -> Exp -> Err Type
-inferBin types env exp1 exp2 = do
-    typ <- inferExp env exp1
-    if elem typ types
-        then
-            checkExp env exp2 typ
-        else
-            fail $ "tipo incorrecto en la expresión " ++ printTree exp1
 
 checkExp :: Env -> Type -> Exp -> Err ()
 checkExp env typ exp = do
@@ -58,6 +113,15 @@ checkExp env typ exp = do
         fail $ "el tipo de " ++ printTree exp ++
         "se esperaba " ++ printTree typ ++
         "pero se encontró " ++ printTree typ2
+
+inferBin :: [Type] -> Env -> Exp -> Exp -> Err Type
+inferBin types env exp1 exp2 = do
+    typ <- inferExp env exp1
+    if elem typ types
+        then
+            checkExp env exp2 typ
+        else
+            fail $ "tipo incorrecto en la expresión " ++ printTree exp1
 
 checkStms :: Type -> Env -> [Stm] -> Err Env
 checkStms type env stms = case stms of
